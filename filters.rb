36becb63a3a4b492e54d9889eeb44b07b2df262f
ealhl <yayt.tuk@gmail.com>
2024-05-02 13:54:59 -0700

    add all functions

--- filters.rb
+++ filters.rb
@@ -3,14 +3,71 @@
 
 def find(id)
     # Your code Here
-  end
+    @candidates.each do |candidate|
+      if candidate[:id] == id
+        return candidate
+      else
+        nil 
+        puts "ID not found"
+      end
+    end
+end
   
   def experienced?(candidate)
     # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
+    if candidate[:years_of_experience] >= 2
+      return true
+    else
+      return false
+    end
   end
   
   # More methods will go below
+  def github?(candidate)
+    if candidate[:github_points] >= 100
+      return true
+    else
+      return false
+    end
+  end
+
+  def languages?(candidate)
+    if candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+      return true
+    else
+      return false
+    end
+  end
+
+  def date?(candidate)
+    if candidate[:date_applied] >= 15.days.ago.to_date
+      return true
+    else
+      return false
+    end
+  end
+
+  def age?(candidate)
+    if candidate[:age] > 17
+      return true
+    else
+      return false
+    end
+  end
+
+  def qualified_candidates(candidates)
+    # Your code Here
+    qualified_candidates = []
+    candidates.each do |candidate|
+      if experienced?(candidate) && github?(candidate) && languages?(candidate) && date?(candidate) && age?(candidate)
+        qualified_candidates << candidate
+      end
+    end
+    return qualified_candidates
+  end
+
+  def ordered_by_qualifications(candidates)
+    # Your code Here
+    candidates.sort_by { |candidate| [candidate[:years_of_experience], candidate[:github_points]] }.reverse
+  end
+
--- main.rb
+++ main.rb
@@ -6,4 +6,11 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+pp find(2)
+pp experienced?(@candidates[0])
+pp github?(@candidates[0])
+pp languages?(@candidates[0])
+pp date?(@candidates[0])
+pp age?(@candidates[0])
+pp qualified_candidates(@candidates)
+pp ordered_by_qualifications(@candidates)
